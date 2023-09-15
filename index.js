a5bd0386a44d35efe5294372a28a4894fe105946
SakuyaYae <89073955+SakuyaYae@users.noreply.github.com>
2023-09-15 15:30:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-var str_arr = ["Sakuya", "Shiraori"];
+function print(input) {
+  console.log(input);
+  console.log();
+};
 
-for (var i = 0; i < str_arr.length; i++) {
-  console.log(str_arr[i]);
+var sakura = null;
+var sakuya = "";
+
+if (sakura > 10) {
+  print(`${sakura} is greater than 10`);
+}
+else if (sakura == null || sakura == "") {
+  print("sakura is null or empty");
+}
+else {
+  print(`${sakura} is not greater than 10`);
 }
 
+switch (sakuya) {
+  case "":
+    print("sakuya is empty")
+    break;
 
+  case null:
+    print("sakuya is null");
+    break;
+
+  default:
+    print("sakuya holds some value");
+}
