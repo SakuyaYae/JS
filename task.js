790d384b33468d540e9955b640f86bc7a9b6404f
SakuyaYae <89073955+SakuyaYae@users.noreply.github.com>
2023-09-20 10:00:52 +0200

    Add files via upload

--- task.js
+++ task.js
@@ -5,8 +5,17 @@ function print(input) {
   console.log();
 }
 
+function check_NaN(value_1, value_2 = 0) {
+  if (isNaN(value_1) || isNaN(value_2)) {
+    return true;
+  }
+  return false;
+}
 function numbers_for() {
   var number = parseFloat(prompt("Type a number: "));
+  if (check_NaN(number)) {
+    return;
+  }
   if (number < 0) {
     number = 0;
     print("Your number was to low: " + number);
@@ -19,6 +28,9 @@ function numbers_for() {
 
 function numbers_while() {
   var number = parseFloat(prompt("Type a number: "));
+  if (check_NaN(number)) {
+    return;
+  }
   if (number < 0) {
     number = 0;
     print("Your number was to low: " + number);
@@ -37,9 +49,12 @@ function odd_numbers_betwhin() {
 }
 
 function even_num_to_choen_num() {
-  var number = parseFloat(prompt("Type a number: "));
   while (true) {
     var error = 0;
+    var number = parseFloat(prompt("Type a number: "));
+    if (check_NaN(number)) {
+      return;
+    }
 
     if (number < 0) {
       print("Your number was to low");
@@ -61,18 +76,86 @@ function even_num_to_choen_num() {
   }
 }
 
-function mulitples(multiplier) {
+function mulitples() {
   var count = parseInt(prompt("Type a count: "));
   var multiplier = parseInt(prompt("Type a multiplier: "));
+
+  if (check_NaN(count, multiplier)) {
+    return;
+  }
+
   for (var i = 0; i <= count; i++) {
     print(i * multiplier);
   }
 }
 
+
+function num_multi_of_three() {
+  var count = parseInt(prompt("Type a count: "));
+
+  if (check_NaN(count)) {
+    return;
+  }
+
+  for (var i = 0; i <= count; i++) {
+    print(i * 3);
+  }
+}
+
+function compare_num_for_largest() {
+  var count = parseInt(prompt("Type a count: "));
+  var temp_value = [];
+  var largest = 0;
+
+  if (check_NaN(count)) {
+    return;
+  }
+
+  for (var i = 0; i <= count; i++) {
+
+    var number = parseInt(prompt(i + ": Type a number: "));
+    temp_value.push(number);
+    if (check_NaN(number)) {
+      return;
+    }
+  }
+  for (var i = 0; i <= count; i++) {
+    for (var j = 0; i < temp_value.length; j++) {
+      if (temp_value[i] > temp_value[j]) {
+        largest = temp_value[i];
+      }
+    }
+  }
+  print(largest);
+}
+
+function compare_untill_zero() {
+  var zero_check = 1;
+  var temp_value = [];
+  var largest = 0;
+
+  while (zero_check != 0) {
+
+    var number = parseInt(prompt("counintes untill zero is typed: Type a number: "));
+    temp_value.push(number);
+    if (check_NaN(number)) {
+      return;
+    }
+  }
+  for (var i = 0; i <= count; i++) {
+    for (var j = 0; i < temp_value.length; j++) {
+      if (temp_value[i] > temp_value[j]) {
+        largest = temp_value[i];
+      }
+    }
+  }
+  print(largest);
+}
+
 var check = true;
 var chose;
 
-var menu = "1 numbers whit for.\n 2 numbers whit while.\n 3 odd number betwhin 0-20. \n 4 even num to choen num. \n 5 Mulupleier"
+var menu = "1 numbers whit for.\n 2 numbers whit while.\n 3 odd number betwhin 0-20. \n 4 even num to choen num. \n 5 Muliplier. \n 6 Multiplier. \n 7 number of multples of three. \n 8 copare numbers for largest. \n 9 compare untill zero and exit \n e, exit, Exit quit program.\n"
 
 while (check) {
   print(menu);
@@ -94,8 +177,21 @@ while (check) {
     case "5":
       mulitples();
 
+    case 6:
+      mulitples();
+
+    case 7:
+      num_multi_of_three();
+
+    case 8:
+      compare_num_for_largest();
+
+    case 9:
+      compare_untill_zero();
+
     case "exit", "Exit", "e":
       check = false;
+
     default:
       print(menu);
 
